411848174bb419a573529a086faae7a4276b9fb4
Saira Shankardass <saira@Sairas-MacBook-Pro.local>
2022-01-22 15:50:14 -0600

    Commiting my completed lab03 files

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -10,36 +10,60 @@ FROM users;
 
 
 -- Exercise 3
+SELECT id, first_name, last_name
+FROM users
+ORDER BY last_name;
 
 
 
 
 -- Exercise 4
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 26;
 
 
 
 
 -- Exercise 5
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id = 26 OR user_id = 12;
 
 
 
 
 -- Exercise 6
-
+SELECT count(*) FROM posts;
 
 
 
 -- Exercise 7
+SELECT user_id, count(*)
+FROM comments
+GROUP BY user_id
+ORDER BY count(*) desc;
 
 
 
 
 -- Exercise 8
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name 
+FROM posts 
+INNER JOIN users
+ON posts.user_id = users.id
+WHERE posts.user_id = 26 or posts.user_id = 12;
 
 
 
 
 -- Exercise 9
+SELECT posts.id, posts.pub_date, following.following_id 
+FROM posts 
+INNER JOIN following
+ON posts.user_id = following.following_id
+WHERE following.user_id = 26;
+
 
 
 
@@ -50,16 +74,37 @@ FROM users;
 
 
 -- Exercise 11
+INSERT INTO bookmarks(user_id, post_id) 
+VALUES(26, 219);
+
+INSERT INTO bookmarks(user_id, post_id) 
+VALUES(26, 220);
+
+INSERT INTO bookmarks(user_id, post_id) 
+VALUES(26, 221);
+
 
 
 
 
 -- Exercise 12
+DELETE FROM bookmarks
+WHERE user_id = 26 AND post_id = 219;
+
+DELETE FROM bookmarks
+WHERE user_id = 26 AND post_id = 220;
+
+
+DELETE FROM bookmarks
+WHERE user_id = 26 AND post_id = 221;
 
 
 
 
 -- Exercise 13
+UPDATE users
+SET email = 'cyoung2022@gmail.com'
+WHERE first_name = 'Cody' AND last_name = 'Young';
 
 
 
